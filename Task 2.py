bca276a2506c1f34fd475f297b8ceb428ab96892
ValeriiaFediuk <fediuk.valeriia14@gmail.com>
2024-03-09 12:23:00 +0200

    task 2 edited

--- Task 2.py
+++ Task 2.py
@@ -2,24 +2,26 @@ def binary_search(arr, target):
     low = 0
     high = len(arr) - 1
     iterations = 0
+    mid = 0
 
     while low <= high:
-        mid = (low + high) // 2
-        if arr[mid] == target:
-            return iterations, arr[mid]
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
         iterations += 1
+        mid = (high + low) // 2
+        if arr[mid] < target:
+            low = mid + 1
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            return iterations, arr[mid]
 
-    if high >= 0:
-        return iterations, arr[high] if arr[high] >= target else arr[high + 1] if high + 1 < len(arr) else None
+    if low < len(arr):
+        return iterations, arr[low]
     else:
-        return iterations, None
+        return iterations, arr[high]
+
 
 arr = [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5]
-target = 6
+target = 1
 iterations, upper_bound = binary_search(arr, target)
 print("Кількість ітерацій:", iterations)
 if upper_bound is not None:
